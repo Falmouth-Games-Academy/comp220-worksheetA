b55ee8e3243e7d7256786438b7a5281eb8ff9ef4
AZ4321 <Arturs.Zingis123@gmail.com>
2018-10-18 14:06:26 +0100

    Added OpenGL functionality
    ps god bless brian for the live coding

--- comp220-OpenGL/main.cpp
+++ comp220-OpenGL/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include "main.h"
 
 int main(int argc, char ** argsv)
 {
@@ -12,13 +13,41 @@ int main(int argc, char ** argsv)
 	}
 
 	//Creating the window, have to remember to quit the window at the end to return the pointer by destroying it(the best way)
-	SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL);
 	if (window == nullptr)
 	{
 		//Show error if SDL didnt create the window
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL_CreateWindow failed", SDL_GetError(), NULL);
 
 		SDL_Quit(); //quit SDL
+		return 1;
+	}
+
+	//Requesting 3.2 Core OpenGL version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GLContext gl_Context = SDL_GL_CreateContext(window);
+	if (gl_Context == nullptr) //if the glew context has a null value, show error and then destroy window
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL_Create_Context failed, check to make sure the context is assigned correctly!", SDL_GetError(), NULL);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
+		return 1;
+
+	}
+
+	//Initialize GLEW
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK) //if Glew is not okay, show error and close window
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Glew start-up failed, check to make sure Glew is started properly!", (char*)glewGetErrorString(err), NULL);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
 		return 1;
 	}
 
@@ -52,13 +81,19 @@ int main(int argc, char ** argsv)
 			}
 		}
 
+		//Rendering goes here, noice
+		glClearColor(1.0, 1.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
 		SDL_GL_SwapWindow(window);
 		SDL_SetWindowResizable(window, SDL_TRUE);
 	}
 
+	//Deleting the context
+	SDL_GL_DeleteContext(gl_Context);
 	//Clean up, deactivating the library and the window
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
 }
+
